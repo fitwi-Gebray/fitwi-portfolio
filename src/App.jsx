06a08880976835749616cab61b55f276f15c3c8b
Fitwi-Gebray <fitwigebray8@gmail.com>
2026-01-19 20:22:46 +0100

    Update README, add reusable components and folder modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+import Hero from "./components/hero/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import About from "./components/About";
